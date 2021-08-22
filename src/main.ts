c47067fcc6dde6b8f60105b92f1651327f27bbea
Armando P. Tamayo <arman2p284@gmail.com>
2021-08-22 16:11:03 -0400

    Add full viewport size
    Add css size to game container element and 100% size to game configuration to get full viewport size

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import Phaser from 'phaser';
 class PlayGame extends Phaser.Scene {
   image: Phaser.GameObjects.Image;
   constructor() {
-    super("PlayGame");
+    super('PlayGame');
   }
   preload() {
     this.load.image('logo', './assets/images/phaser.png');
@@ -12,17 +12,18 @@ class PlayGame extends Phaser.Scene {
     this.image = this.add.image(400, 300, 'logo');
   }
   update() {
-    this.image.rotation += 0.01;
+    // this.image.rotation += 0.01;
   }
 }
 
 const configObject: Phaser.Types.Core.GameConfig = {
+  backgroundColor: '#111111',
+  type: Phaser.AUTO,
+  parent: 'game_container',
+  width: '100%',
+  height: '100%',
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    parent: 'game_container',
-    width: 800,
-    height: 600
   },
   scene: PlayGame
 };
